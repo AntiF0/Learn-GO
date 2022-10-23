a180fd85178b898996e7206c695c406679635f1f
AntiF0 <AntiF0@users.noreply.github.com>
2022-10-23 22:14:11 +0800

    feat: 更新了模块引用

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,12 @@
 package main
 
 import (
+	"example.com/greetings"
 	"fmt"
-	"greetings"
-
-	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
-
+	// Get a greeting message and print it.
 	message := greetings.Hello("Gladys")
 	fmt.Println(message)
 }
