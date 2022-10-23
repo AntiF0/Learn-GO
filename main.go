991a8aae56f91ec4b5da8d66e78cba3922cd0812
AntiF0 <AntiF0@users.noreply.github.com>
2022-10-23 17:48:39 +0800

    feat

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"example.com/greetings"
 	"fmt"
- "rsc.io/quote"
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
